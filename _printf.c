65f263217f66f42e88c113d6ab9f87e901a27fb6
ybax01 <youssef1002016@gmail.com>
2024-07-30 14:41:39 +0100

    az

---
+++ _printf.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * _printf - A custom function to replicate "printf" from the standard library
+ * @format: the main string that gets printed
+ *
+ * Return: num of characters printed except the null byte
+ */
+int _printf(const char *format, ...)
+{
+	int i, count;
+	char c;
+	char *s;
+
+	va_list args;
+
+	va_start(args, format);
+
+	if (!format)
+		return (-1);
+
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					c = va_arg(args, int);
+					_putchar(c);
+					count++;
+					break;
+				default:
+					_putchar('%');
+					_putchar(format[i]);
+					count = count + 2;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+
+	return (count);
+}
